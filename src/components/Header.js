552771c4d15b239ac5ccb4d767f9fd908825b34f
Sam Rogers <samrogers15@gmail.com>
2021-08-24 08:59:39 -0700

    updating Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,40 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { Typography, Avatar, Grid, Box } from "@material-ui/core";
-import fries from '../fries.png';
+import fries from "../fries.png";
+
+// CSS STYLES
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    margin: theme.spacing(1),
+  },
+  title: {
+    color: "black",
+  },
+  textContainer: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "100vw",
+    textAlign: "center",
+    zIndex: 1,
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
+
   return (
-    <Box>
-      <Avatar src={fries} alt="French Fries" />
+    <Box className={classes.textContainer}>
+      <Grid container justify="center">
+        <Avatar className={classes.avatar} src={fries} alt="French Fries" />
+      </Grid>
+      <Typography className={classes.title} variant="h4">
+        Matt Coburn Sound
+      </Typography>
     </Box>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Appbar from './Appbar';
 import Header from './Header';
+import Particles from 'react-particles';
 
 const Home = () => {
   return (
     <div>
       <Appbar/>
       <Header/>
+      <Particles 
+        params={{
+          particles: {
+            number: {
+              value: 45
+            }
+          }
+        }}
+      />
     </div>
   );
 };
